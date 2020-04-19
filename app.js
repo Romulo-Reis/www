320be7a53024d4a0b27c2e93e6810ea27834c7d9
Romulo-Reis <romuloreistavates@hotmail.com>
2020-04-18 21:06:02 -0300

    Servindo arquivos estáticos.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+var path = require("path");
+
 app.get("/",function(req,res){
-    res.send("Olá mundo!");
+    res.sendFile(__dirname+"/public/page/index.html");
 });
 
 app.listen(8080, function(){
